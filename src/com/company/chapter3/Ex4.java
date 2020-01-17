2634884ac5a31f507eb63856103489655778cf67
r.ushakov <rushakov@gmail.com>
2020-01-17 17:04:35 +0300

    17.01.2020:Ex4

---
+++ src/com/company/chapter3/Ex4.java
@@ -0,0 +1,9 @@
+package com.company.chapter3;
+
+public class Ex4 {
+    public static void main (String[] args){
+        for (int i = 1000; i>=0; i -= 2){
+            System.out.println(i);
+        }
+    }
+}
